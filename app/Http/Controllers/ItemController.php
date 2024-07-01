1be43b10ce7018ac4e6fd07aac6bdfc0a36afd68
mabuchi428 <s0800306@gmail.com>
2024-07-01 19:06:51 +0900

    変えた

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -37,10 +37,11 @@ class ItemController extends Controller
         // POSTリクエストのとき
         if ($request->isMethod('post')) {
             // バリデーション
-            $this->validate($request, [
-                'name' => 'required|max:100',
-            ]);
-
+                $this->validate($request, [
+                    'name' => 'required|max:100',
+                    'type' => 'max:100',
+                    'detail' =>'max:100'
+                    ]);
             // 商品登録
             Item::create([
                 'user_id' => Auth::user()->id,
